8ef3a4bcd42e5244487d0bc13f23a6cc336007bf
unknown <you@example.com>
2025-01-07 15:28:56 +0100

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,13 +6,13 @@ const Hero = () => {
 
   return (
     <section id="home" className="overflow-hidden">
-      <div className="min-h-screen  relative flex md:flex-row flex-col-reverse md:items-end justify-left items-center ml-[10%]" >
+      <div className=" relative flex md:flex-row flex-col-reverse md:items-end justify-left items-center ml-[10%]" >
         <div
           data-aos="slide-left"
           data-aos-delay="1200"
           className="absolute h-full md:w-4/12 w-8/12 top-0 right-0 bg-primaryLinear bottom-0 -z-10"
         >
-          <h1 className="rotate-90 hidden md:block md:absolute top-[40%] right-[-15%] text-[#EAF2FA]">
+          <h1 className="rotate-90 hidden md:block md:absolute p-[40%] right-[-15%] text-[#EAF2FA]">
             {hero.firstName}{" "}
             <span className="text-dark_primary">{hero.LastName}</span>
           </h1>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,7 +14,7 @@ const Projects = () => {
   const { Projects } = content;
   return (
     <section className="bg-bg_light_primary" id="projects">
-      <div className="md:container px-5 pt-14 min-h-screen flex flex-col justify-between">
+      <div className="md:container px-5 min-h-screen flex flex-col justify-between">
         <div>
           <h2 className="title" data-aos="fade-down">
             {Projects.title}
