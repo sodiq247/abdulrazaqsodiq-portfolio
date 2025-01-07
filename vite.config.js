0a7f92ce74dd23bad2a163cb4ac1bc24cc771cc1
unknown <you@example.com>
2025-01-07 11:00:47 +0100

    build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Explicitly specify the output directory
+    emptyOutDir: true, // Clear the dist folder before building
+  },
+  server: {
+    port: 3001, // Optional: Specify a dev server port
+  },
 })
